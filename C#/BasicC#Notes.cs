6a69944f053f9a98c760ee31d9a20869b4ec6442
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-03-18 21:41:26 +0800

    changed name from BasicCSharp to Basic CSharpNoes

--- C#/BasicC#Notes.cs
+++ C#/BasicC#Notes.cs
@@ -3,6 +3,7 @@
 // By: RECIO, Angelo Rafael
 
 //NOTE: Study dotnet commands in cmd
+//TEst other C# stuff with https://try.dot.net/
 
 namespace Basics{
     class Program{
@@ -34,19 +35,26 @@ namespace Basics{
                 //Convert.ToIntDouble
             
                 //var: a variable that auto detects what data type it is declared and initialized as.
-            var any = "string" 
+            var any = 69 ;
                 // var cannot be declared in one line and initialized in another.
 
                 //adding "const" before a variable makes it immutable
-            const double lit = 420.69
+            const string lit = "HAHA XD";
                 // const variable must also be initialized and declared in the same line
 
-                //Arithmetic Operations:
+                //ARITHMETIC OPERATORS:
                 // +:Addition, -:Subtraction, *:Multiplication, /:Division, %:Modulus
                 //NOTE: Division of integers will output integer quotient. if you want to change the answer to show decimals, declare the answer as a double
                 //you can manage the hoe the expression is evaluated by grouping the expression, 1+3*5 in not equal to 1+(3*5)
-                
-        }
+            
+                //ASSIGNEMT OPERATORS
+            any += 2;
+                //arithmetic operators can be use for assignment
+
+                //Increment Operator: used to decrease or increase a value by 1
+            any++; //prefix - increments/decrements value, value isn't change
+            ++any; //postfix - increments/decrements value first before using it
+    }
     }
 }
 
