59cface29c29e77db260c5e43747fac669d6deaa
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-03-24 20:50:41 +0800

    added ES6 Object Notes

--- Javascript/ES6Notes.js
+++ Javascript/ES6Notes.js
@@ -26,9 +26,9 @@ add2numbers(1,2);
 const salutation = x => "Welcome " + x; //you can also declare the values of parameters at the function
 console.log(salutation("Angelo")) //calling anf outputting single-line function
     //new Syntax makes it easy to use in inline function. Ex: use the function in each element of an array.
-var arr = [2,3,7,8];
-//arr.forEach(function(el){console.log(el*2);}); //for each element a function is used in it
-//arr.forEach(v => {console.log(v*2);}); //new syntax
+var array = [2,3,7,8];
+//array.forEach(function(el){console.log(el*2);}); //for each element a function is used in it
+//array.forEach(v => {console.log(v*2);}); //new syntax
 
 
     //LOOPS
@@ -47,4 +47,64 @@ const dict = {a: 1, b: 2, c: 3};
 for (const [key,value] of Object.entries(dict)){
     console.log(key, value);
 }
-    
+
+    //ES6 Objects - Variables can be object data types that contain many properties
+    //they can also have functions as properties called "methods"
+let knight = {
+    ATK: 100,
+    DEF: 50,
+    SPD: 50,
+    AtkBoost(){ //methods: functions inside  objects
+        this.ATK += 20;
+    }
+};
+knight.AtkBoost(); //called the method to work
+//console.log(knight.ATK); //prints 120
+    //you can also value shorthand when initializing properties using the variable with the same name
+let color = "green";
+let habitat = "Tropical";
+let NarraTree = {
+    color, //from variable outside the block
+    habitat //from variable outside the block
+}
+//console.log(NarraTree.habitat); //prints tropical
+    //when an object has multiple duplicate property names, the last value/property will override the others and will be printed
+let re = {x:1,x:2,x:3,x:4}
+//console.log(re.x)
+
+    //computer property names: uses [] notation
+let title = 'name';
+let id = 1234;
+let status = "single";
+let user = {
+    [title]:'Dave', // ([title] = name):'Dave'
+    [`user${id}`]: `${status}` // ([`user${id}`]=user1234) : (`${status}`=456)
+};
+console.log(user.name) //prints Dave
+console.log(user.user1234) //prints single
+    //Object.assign() - makes allow to uses multiple sources into one target to create a new object
+let person = {
+    name:"Jack",
+    age:20 ,
+    sex:'Male'
+};
+let personality = {
+    favMusic : "HipHop",
+    favMovie : "LOTR",
+    hobby : "Bike"
+};
+let newStudent = Object.assign({}, person, personality); // gets from person and personality objects to form a new object
+//console.log(newStudent.age) //prints 20
+//console.log(newStudent.favMovie) //prints LOTR
+    // We use assign() to create a duplicate object without referencing to the bas object
+let guy = {
+    name : "Jack",
+    age : 18
+};
+let newGuy = Object.assign({},guy)
+newGuy.name = "Bob";
+console.log(guy.name); //prints Jack
+console.log(newGuy.name); //prints Bob
+    //You can also assign the value of an object property in the Object.assign() statement
+let newGuy2 = Object.assign({},guy, {name:"kyle"});
+console.log(newGuy2.name);
