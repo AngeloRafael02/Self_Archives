b74e4b28fadfacc05e19346d514f4f67b6ba5dca
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-03-27 20:35:07 +0800

    added array, array properties and method notes

--- Javascript/OOPJavascript.js
+++ Javascript/OOPJavascript.js
@@ -15,17 +15,19 @@ var call2 = me['name'];
     //console.log(call); //prints Angelo
     //console.log(call2) //prints Angelo
     
-    
+    //Object Initialization
     //CONSTRUCTOR: written function used to crate new objects
 function person(name,age,color){
     this.name = name;
     this.age = age;
     this.color = color;
 }
+
 var p1 = new person("Angelo", 20, "gray"); //new objects
 var p2 = new person("Lianna", 21, "pink");
 console.log(p1.age); //prints p1's age
 console.log(p2.name); //prints p2's name\
+
 
     //Object Initialization: use the object literal or initializer syntax to create single objects
 var John = {name: "John", age:25}
@@ -34,6 +36,7 @@ var James = { //line breaks and indents don't matter
     age:21
 }   //accessing Object properties:
 console.log(John.age)
+
 
     //METHODS: you can also define a funciton outside a constructor and associate it with an object
 function fish(name, color,number){
@@ -45,5 +48,21 @@ function fish(name, color,number){
 function twicer(){
     return this.number * 2;
 }
-var goldfish = new fish("lenny","orange",25);
-console.log(goldfish.twicer());
+var goldfish = new fish("lenny","orange",25); //new object
+console.log(goldfish.twicer()); //called a method within the object
+
+
+    //ARRAYS: stores multiple values in a single variable
+    //Useful in scaling values
+var courses = new Array ("Python","C#","Javascript","SQL"); //<= array literal syntax
+//console.log(courses[1]); //Access an item in an array with its index via square brackets
+var framework = new Array(3); //<= Alternative Syntax
+    framework[0] = 'ReactJS';
+    framework[1] = 'Angular';
+    framework[2] = 'VueJS';
+
+
+    //Array Properties & Methods
+console.log(courses.length); // .length - returns the number of elements
+var toLearn = courses.concat(framework); // .concat - concatenates arrays
+console.log(toLearn[5]);
