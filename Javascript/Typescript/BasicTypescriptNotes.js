3bb246f7773bf2b3bcc85c0508e3f62ca7e86888
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-03-29 21:09:22 +0800

    started Typescript notes

---
+++ Javascript/Typescript/BasicTypescriptNotes.js
@@ -0,0 +1,12 @@
+//Basic Typescript Notes
+//By: Angelo Rafael Recio
+//VARIABLES
+var isSingle = true; //const: immutable variables
+var number = 68;
+var message = 'Hello Typescript';
+console.log(message);
+//FUNCTIONS
+var getFullName = function (name, surname) {
+    return name + ' ' + surname;
+};
+getFullName("Angelo", "Recio");
