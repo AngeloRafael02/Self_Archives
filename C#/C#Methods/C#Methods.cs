522aee816719a99b8a3972a86b20761e950dece2
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-03-29 21:22:07 +0800

    added variable referencing in Notes

--- C#/C#Methods/C#Methods.cs
+++ C#/C#Methods/C#Methods.cs
@@ -18,16 +18,23 @@ namespace Angelo{
         static int Area(int h, int w){
             return h * w;
         }  
-        static void Main(string[] args){
-            sayHi();//call a method to the main method
-            sayHi();//can be done multiple times
-            twice(2); //calls a function with an argumets to be used 
-            Console.WriteLine(Power(3));
-            Console.WriteLine(Power(2, 3));//default arguments can be override by new value
-
-            int res = Area(w: 5, h: 8); //Arguments can be rearranged as long as assigned correctly
-            Console.WriteLine(res);
-
+        static void Sqr(ref int x){ // we can pass copies of an memories address into a formal parameter
+            x = x * x;
         }
+            static void Main(string[] args){
+                sayHi();//call a method to the main method
+                sayHi();//can be done multiple times
+                twice(2); //calls a function with an argumets to be used 
+                Console.WriteLine(Power(3));
+                Console.WriteLine(Power(2, 3));//default arguments can be override by new value
+
+                int res = Area(w: 5, h: 8); //Arguments can be rearranged as long as assigned correctly
+                Console.WriteLine(res);
+
+                int a = 3; // variable name is "a"
+                Sqr(ref a); // "a"'s address is referenced to be used in a method that requires an "x" variable
+                Console.WriteLine(a); //returns new value from being passed down and processed by references
+
+            }   
     }
 }
