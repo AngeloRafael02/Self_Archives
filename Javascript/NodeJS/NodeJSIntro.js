f0d98cda7f3cf71d28cbe3bca1e630bd97d95219
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-04-11 23:22:07 +0800

    added comments for note taking

--- Javascript/NodeJS/NodeJSIntro.js
+++ Javascript/NodeJS/NodeJSIntro.js
@@ -2,9 +2,10 @@
     //By: Angelo Rafael Recio
 
     //declare a variable to be called for when we use http module
-var http = require('http'); //the require() function is used to include a module wit the name of the module. http is built-in
+var http = require('http'); //the require() function is used to include a module wit the name of the module. http is built-in to allow transfer data over http
 
-http.createServer(function(req,results){
-    results.writeHead(200, { 'Content-Type' : 'text/html' });
-    results.end("Hello World!");
-}).listen(8080);
+http.createServer(function(req,res){ //.createServer() - method used to create an HTTP Server
+    res.writeHead(200, { 'Content-Type' : 'text/html' }); //If the response from the HTTP server is supposed to be displayed as HTML, you should include an HTTP header with the correct content type:
+    res.write('Hello NodeJS'); //write a response to the client
+    res.end(); //end the response
+}).listen(8080); //server object listens to port 8080
