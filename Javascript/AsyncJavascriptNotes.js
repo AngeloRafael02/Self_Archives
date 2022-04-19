481d8b94f98cda951a830a090aa253796b90c761
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-04-19 22:18:33 +0800

    added async and await notes

--- Javascript/AsyncJavascriptNotes.js
+++ Javascript/AsyncJavascriptNotes.js
@@ -56,7 +56,7 @@ let order = (fruit_name, call_production) =>{
     //THIS IS CALLED CALLBACK HELL, we dont want that
     //thats why we use PROMISES to escape callback Hell
     //Previous function but in Promises format: using same stocks variables
-let is_shop_open = true;
+let is_shop_open = false;
 let order1 = ( time, work ) => {
     return new Promise( ( resolve, reject )=>{
         if( is_shop_open ){
@@ -65,36 +65,73 @@ let order1 = ( time, work ) => {
         }, time);
         }
         else{
-        reject( console.log("Our shop is closed") )
+            reject( console.log("Our shop is closed") )
         }
     })
     }
     //Function was called here -V
 order1(2000,()=>console.log(`${stocks.Fruits[0]} was selected`))
-.then(()=>{
-    return order(0000,()=>console.log('production has started'))
-})
-.then(()=>{
-    return order(2000, ()=>console.log("Fruit has been chopped"))
-})
-.then(()=>{
-    return order(1000, ()=>console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`))
-})
-.then(()=>{
-    return order(1000, ()=>console.log("start the machine"))
-})
-.then(()=>{
-    return order(2000, ()=>console.log(`ice cream placed on ${stocks.holder[1]}`))
-})
-.then(()=>{
-    return order(3000, ()=>console.log(`${stocks.toppings[0]} as toppings`))
-})
-.then(()=>{
-    return order(2000, ()=>console.log("Serve Ice Cream"))
-})
-
+.then(()=>{return order(0000,()=>console.log('production has started'))})
+.then(()=>{return order(2000,()=>console.log("Fruit has been chopped"))})
+.then(()=>{return order(1000,()=>console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`))})
+.then(()=>{return order(1000,()=>console.log("start the machine"))})
+.then(()=>{return order(2000,()=>console.log(`ice cream placed on ${stocks.holder[1]}`))})
+.then(()=>{return order(3000,()=>console.log(`${stocks.toppings[0]} as toppings`))})
+.then(()=>{return order(2000,()=>console.log("Serve Ice Cream"))})
+.catch(()=>{console.log("Customer left")}) //catch procs when the reject() function is proced
+.finally(()=>{console.log("end of day\n")}) //finally is called after when either the resolve() or the reject() is called
     
+    // ASYNC,AWAIT:
+function time(ms) {
 
+    return new Promise( (resolve, reject) => {
+
+        if(is_shop_open){
+            setTimeout(resolve,ms);
+        }
+
+        else{
+            reject(console.log("Shop is closed"))
+        }
+    });
+}
+async function kitchen(){
+    try{
+        await time(2000)
+        console.log(`${stocks.Fruits[0]} was selected`)
+    
+        await time(0000)
+        console.log("production has started")
+    
+        await time(2000)
+        console.log("fruit has been chopped")
+    
+        await time(1000)
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`)
+    
+        await time(1000)
+        console.log("start the machine")
+    
+        await time(2000)
+        console.log(`ice cream placed on ${stocks.holder[1]}`)
+    
+        await time(3000)
+        console.log(`${stocks.toppings[0]} as toppings`)
+    
+        await time(2000)
+        console.log("Serve Ice Cream")
+        }
+    
+        catch(error){
+            console.log("customer left")
+        }
+    finally{
+        console.log("Day ended, shop closed")
+        }
+    }
+    
+    // Trigger
+//kitchen();
 
     //ASYNCHRONOUS 
 setTimeout(function(){delayed("I'm Delayed");}, 3000); //use SetTimeout to delay a function or event for a certain amount of time
