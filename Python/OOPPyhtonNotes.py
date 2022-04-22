7544e8b6099c54bdf226d596b0c2e89cf7a1fc41
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-04-22 23:24:33 +0800

    fixed minor typos

--- Python/OOPPyhtonNotes.py
+++ Python/OOPPyhtonNotes.py
@@ -2,7 +2,7 @@
     #RECIO, Angelo Rafael
 
 
-    #   CLASS: describes whatan object will be but is separate from the object itself, also called an instance?
+    #   CLASS: describes what an object will be but is separate from the object itself, also called an instance?
     #   can be described as the object's blueprint, or description
 class Cat:
     #Methods, function inside class, all methods have a "self" argument
@@ -18,7 +18,7 @@ class Animal:
     def __init__(self,name,color): #Superclass: Class the is inherited from
         self.name = name
         self.color = color
-class Bird(Animal):
+class Bird(Animal): #Subclass: Class that Inherits from another Class
     def BirdSound(self):
         print("Tweet!")
 class Dog(Animal): #Subclass: Class that Inherits from another Class
