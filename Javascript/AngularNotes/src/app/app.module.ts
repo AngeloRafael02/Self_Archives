e19e989825142ea744f96f9126aaa400990eeaba
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-04-23 22:05:09 +0800

    added new componenet for class binding notes

--- Javascript/AngularNotes/src/app/app.module.ts
+++ Javascript/AngularNotes/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 
 import { ChildComponent } from './child/child.component';
 import { Child1Component } from './child1/child1.component'
+import { Child2Component } from './child2/child2.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ChildComponent,
-    Child1Component
+    Child1Component,
+    Child2Component
   ],
   imports: [
     BrowserModule
