84beee9383ce47d667d5393481014b2431f09eba
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-05-06 22:28:27 +0800

    added sample for wildcat routing

--- Javascript/AngularNotes/src/app/app-routing.module.ts
+++ Javascript/AngularNotes/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { RoutingSampleComponent } from './routing-sample/routing-sample.component';
+import { WildcardRouteComponent } from './wildcard-route/wildcard-route.component';
 
 const routes: Routes = [
-  {path: "routing-Sample", component: RoutingSampleComponent } //we can add more component with their new path, as opposed of module.ts handling all the components
+  {path:"", redirectTo:'routing-Sample', pathMatch:"full"}, //default is at the top, REDIRECT, when url is ''
+  {path: "routing-Sample", component: RoutingSampleComponent }, //we can add more component with their new path, as opposed of module.ts handling all the components
+  {path: "**", component:WildcardRouteComponent}, //<= for when error 404. NOTE: Wildcard rout should be the last one in the array
 ];
 
 @NgModule({
@@ -16,4 +19,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [RoutingSampleComponent]
+export const routingComponents = [RoutingSampleComponent, WildcardRouteComponent]
