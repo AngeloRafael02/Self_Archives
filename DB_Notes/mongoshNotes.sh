6aac98f4c9fe2c2430448ce6f2475732968acc3f
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-30 21:05:30 +0800

    added notes in deleting documents

--- DB_Notes/mongoshNotes.sh
+++ DB_Notes/mongoshNotes.sh
@@ -8,7 +8,11 @@ on 1 --username RecioAngeloRafael # then you will be prompted to enter your pass
 quit(); # To Quit MongoSH
 
 db # shows current DB
+show dbs # show databases
+use <dbName> # Connect to database
 show collections #shows collections
+load("JavascriptFile.js") #run Javascript
+
 db.createUser({ # To create a User | NOTE: Study on MongoDB Roles
     user:"brad",
     pwd:"password",
@@ -16,9 +20,19 @@ db.createUser({ # To create a User | NOTE: Study on MongoDB Roles
 });
 db.createCollection('CollectionName'); # To Create A Collection (Table)
 db.<CollectionName>.drop(); # To Drop a Colection
+
+        #CRUD
+    #CREATE
 db.<CollectionName>.insertOne({key:"value",...}) #To insert One Document 
 db.<CollectionName>.insertMany([{key:"value1"},{key:"value2"},...]) # To Insert Many Document
+    #READ
 db.<CollectionName>.find(); # to show all documents
-db.<CollectionName>.update({keyMatch;"ValueMatch"},{key:"newValue", ...}); #To update Document
-db.<CollectionName>.update({keyMatch;"ValueMatch"},{$set:{key:"newValue", ...}}); # Use $set to keep existing data when updating
-    #NOTE update() is depreciated, use updateOne() to update the first to be find or updateMany() to update multiple documents 
+    #UPDATE
+db.<CollectionName>.update({keyMatch;"ValueMatch"},{key:"newValue", ...}); #To update Document, re-writing the whole document
+db.<CollectionName>.update({keyMatch;"ValueMatch"},{$set:{key:"newValue", ...}}); # Use $set to keep existing data when updating/adding new field
+db.<CollectionName>.update({keyMatch;"ValueMatch"},{$unset:{key:"newValue", ...}}); # Use $unset to remove a field in a document
+        #NOTE update() is depreciated, use updateOne() to update the first to be find or updateMany() to update multiple documents 
+    #DELETE
+db.<CollectionName>.remove({ketMatch:"ValueMatch"}) # Delete all qualifying the condition
+db.<CollectionName>.remove({ketMatch:"ValueMatch"}, {justOne: true})
+db.<CollectionName>.findOneAndDelete({ketMatch:"ValueMatch"})
