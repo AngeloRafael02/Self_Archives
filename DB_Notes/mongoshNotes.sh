4d3c74f8544b4db868c76b55db32b235213c981f
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-07-20 21:06:12 +0800

    renewed Connection URI

--- DB_Notes/mongoshNotes.sh
+++ DB_Notes/mongoshNotes.sh
@@ -3,8 +3,8 @@
     # https://www.mongodb.com/docs/ for more documentation
     
     #To directly Connect to database:
-mongosh "mongodb+srv://cluster0.cbzs3.mongodb.net/GI_DB" --apiVersi
-on 1 --username RecioAngeloRafael # then you will be prompted to enter your password 
+mongosh "mongodb+srv://cluster0.cbzs3.mongodb.net/GI_DB" --apiVersion 1 --username RecioAngeloRafael # then you will be prompted to enter your password 
+XPWy77tpEFjHWuy
 quit(); # To Quit MongoSH
 
 db # shows current DB
@@ -12,7 +12,6 @@ show dbs # show databases
 use <dbName> # Connect to database
 show collections #shows collections
 load("JavascriptFile.js") #run Javascript
-
 db.createUser({ # To create a User | NOTE: Study on MongoDB Roles
     user:"brad",
     pwd:"password",
@@ -28,7 +27,7 @@ db.<CollectionName>.insertMany([{key:"value1"},{key:"value2"},...]) # To Insert 
     #READ
 db.<CollectionName>.find(); # to show all documents
     #UPDATE
-db.<CollectionName>.update({keyMatch;"ValueMatch"},{key:"newValue", ...}); #To update Document, re-writing the whole document
+db.<CollectionName>.update({keyMatch;"ValueMatch"},{key:"newValue", ...}); #To update Document, re-writing the whole document so you may need to rewrite all of the content of the document
 db.<CollectionName>.update({keyMatch;"ValueMatch"},{$set:{key:"newValue", ...}}); # Use $set to keep existing data when updating/adding new field
 db.<CollectionName>.update({keyMatch;"ValueMatch"},{$unset:{key:"newValue", ...}}); # Use $unset to remove a field in a document
         #NOTE update() is depreciated, use updateOne() to update the first to be find or updateMany() to update multiple documents 
