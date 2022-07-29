fe9e93b1c595d8b30bee43da8b7c546c55e413d7
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-07-29 20:46:29 +0800

    Added Notes on Using Numpy on Pandas DataFrames

--- Python/MachineLearning/MachineLearningNotes.py
+++ Python/MachineLearning/MachineLearningNotes.py
@@ -16,7 +16,22 @@ import pandas as pd
 df = pd.read_csv('https://sololearn.com/uploads/files/titanic.csv')
 print(df.head()) #head() returns first 5 rows of the DataFrame + its Header 
 print(df.describe()) #returns Statistics of the columns (mean, standard deviation, etc. )
+
 series = df[['Age','Sex']] #Series - Column/s from th DataFrame
 print(series.head())
+
 df['isMale'] = df['Sex'] == 'male' #Adds a boolean series with its condition
 print(df.head())
+    #Making Numpy Arrays on a DataFrame
+MultiDimArray = df[['Fare','Age']].values
+print(MultiDimArray) # Prints all the values of a Series/Column in an ARRAY
+shapeOfArray = MultiDimArray.shape
+print("Array Shape: " + str(shapeOfArray)) # Prints the shape of the array (rows, columns)
+    #Select A single Element be specifying the array you want + condition
+    #Array Syntax: [row:column:order] <condition>
+print(MultiDimArray[0,1]) # Prints the value of the first row and second column
+print(MultiDimArray[:,1] > 18) # Prints all the boolean values of the second column that are less than 18
+arr = df[['Pclass','Fare','Age']].values[:10] # Selects the first 10 rows of the array
+mask = arr[:,2] < 18 # Selects the boolean values of the third column that are less than 18
+print(arr[mask]) # Prints the values of the array that are less than 
+print(mask.sum()) # Prints the number of values that are less than 18
