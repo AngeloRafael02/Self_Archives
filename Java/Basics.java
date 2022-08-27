f0fe3f78c29ff82603b17a4fe44a3441a2ee27b4
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-08-27 21:12:45 +0800

    Added new Language: Java (started on Variables)

---
+++ Java/Basics.java
@@ -0,0 +1,15 @@
+public class Basics{
+    public static void main(String[] args){
+        String name = "Angelo";
+        System.out.println(name);
+
+        int number = 69;
+        System.out.println(number);
+
+        double score = 4.2;
+        System.out.println(score);
+
+        char letter = 'Z';
+        System.out.println(letter);
+    }
+}
