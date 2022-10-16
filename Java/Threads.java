51e075d48e82c8b20677f788791c16838e95a534
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-10-16 21:58:49 +0800

    Added notes on declaring Thread classes

--- Java/Threads.java
+++ Java/Threads.java
@@ -2,15 +2,26 @@
     * BY: Angelo Rafael Recio
     */ 
 
-    //Add notes on these
-class Loader implements Runnable{
+    //Java is a multi-threading language. multiple components can be run.
+    // 1ST WAY - to create Thread Class, just a class inherit it
+class Loader extends Thread{
     public void run(){
-        System.out.println("Hello");
+        System.out.println("Hello, from class with inherited Thread");
     }
 }
+    // 2ND WAY - Implement a runnable interface
+class Loader2 implements Runnable{
+    public void run(){
+        System.out.println("Hello, from class with implemented Runnable Interface");
+    }
+}
+
 class Threads{
     public static void main(String[] args) {
-        Thread t = new Thread(new Loader());
+        Loader t = new Loader();
         t.start();
+
+        Thread s = new Thread(new Loader2());
+        s.start();
     }
 }
