dce22db454883b97b5e995dd09e429c669cd9d76
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-10-28 20:41:49 +0800

    Added Basic GDScript syntaxes

---
+++ GDScript/Basics .gd
@@ -0,0 +1,57 @@
+  # DGScript Basics
+  # By: Angelo Rafael F. Recio
+
+    # DGScript - Similar to Python with some typescript influences
+
+    #Variables
+export var age:int = 21;
+const NAME = "Angelo";
+var arr:int[] = [1,2,3,4];
+var dictionary = {a:1, b:2}
+var typed_var: int;
+var inferred_type: = "String"
+
+    #Enums
+enum {UNIT_NEUTRAL, UNIT_ENEMY, UNIT_ALLY}
+enum Named {THING_1, THING_2, ANOTHER_THING = -1}
+
+  #Built-In Vector Types
+var v2 = Vector2(1,2)
+var v3 = Vector3(1,2,3)
+
+  #Function
+func functionOne(pamam1,param2):
+  var local_var  = 5
+  
+  # If-Elif-Else syntax
+  if param1 < loval_var:
+    print(param1)
+  elif param2 > loval_var++:
+    print(param2)
+  else 
+    print("???")
+  
+  # For Loop
+  for i in range(10):
+    print(i)
+
+  #While Loop
+  while param != 0:
+    param2 -= 1
+  
+  var local_var2 = param1 + 3
+  return local_var2
+
+# Functions override functions with the same name on the base/parent class.
+# If you still want to call them, use '.' (like 'super' in other languages).
+func something(p2,p1):
+  .something(p1,p2)
+
+  # Class
+class Class:
+  static var a = 10
+  var b = 12
+  #Constructor
+  func _init():
+    var lv = something.new()
+    print(lv.a)
