48cb707c3c43bf7f2290ab983b4db129b7d73425
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-02-11 20:35:21 +0800

    added first request handller

--- Python/Django/app/playground/views.py
+++ Python/Django/app/playground/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 # used for request handling
+
+def sayHi(request):
+    return HttpResponse('Sup')
