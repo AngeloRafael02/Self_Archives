16d591f4c5082e8dfdd9519781cb4c2db64d37d3
Angelo Rafael Recio <recioangelorafael@gmail.com>
2023-04-20 20:54:40 +0800

    Created .sh file for mysql cli

---
+++ DB_Notes/MySQL_CLI.sh
@@ -0,0 +1,15 @@
+
+#POSTGRESQL commands
+
+mysql -u <username> <DBName> #login via username into a certain database, prompts password input
+#NOTE: make sure that the mysql service is running to work
+\h          #or \help - Print help about MySQL Shell
+\q          #to quit
+\js         #switch execution mode to javascript
+\py         #switch execution mode to python
+\sql         #switch execution mode to SQL
+
+SHOW DATABASES;                 #List all databases
+SHOW DATABASES LIKE "string"    #list all databases with the same name 
+SHOW SCHEMAS;                   #List all schemas 
+USE <DatabaseName>              #switch to another DB
